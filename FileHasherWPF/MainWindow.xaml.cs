62c03501cdef9ff4302e20c3d7858daedc59f250
Nt.gm79sp <gm79sp@qq.com>
2017-12-22 23:54:00 +0800

    重写的前夜

--- FileHasherWPF/MainWindow.xaml.cs
+++ FileHasherWPF/MainWindow.xaml.cs
@@ -39,47 +39,71 @@ namespace FileHasherWPF
 #endif
         }
 
-        #region 控制逻辑
+        #region 文件的控制逻辑
+        // 当前任务列表与文件读取进度
+        List<GetFileHash> hashList;
+        bool isProgressing = false;
+        long totalFileBytes = 0L;
+        long currentFileBytes = 0L;
+
+        // 基于所有任务（处理队列）的进度条更新
+        // 数据绑定是基于事件的，每一次属性的变更传递事件到控件，而不是由控件本身来主动、定时访问。
+        // 既然文件哈希的过程是确定的，那么此处从简设计，使用固定的更新频率来显示进度条
+        async Task UpdateProgressBar()
+        {
+            while (currentFileBytes < totalFileBytes)
+            {
+                currentFileBytes = 0L;
+                foreach (var hash in hashList)
+                {
+                    currentFileBytes += hash.GetCurrentBytesPosition();
+                }
+                progressBar.Value = currentFileBytes;
+                Task.Delay(100);
+            }
+            isProgressing = false;
+        }
+
+        async Task FileTask(string f)
+        {
+#if DEBUG
+            var timer = new System.Diagnostics.Stopwatch();
+            timer.Start();
+#endif
+            var fileTask = new GetFileHash(hashType, f);
+            hashList.Add(fileTask);
+            totalFileBytes += fileTask.FileLength;
+            AddResult(isFullPath ? f : fileTask.FileName, fileTask.HashResult);
+#if DEBUG
+            timer.Stop();
+            textBox_Stream.AppendText("耗时：" + timer.Elapsed.ToString() + nl + nl);
+#endif
+        }
+
+        private void AddResult(string name, string result)
+        {
+            if (result != GetFileHash.FILE_ERROR)
+            { textBox_HashCode.Text = result; }
+            textBox_Stream.AppendText(name + nl + result + nl + nl);
+        }
+
         // 处理打开多个文件的逻辑控制
         // 异步调用哈希算法，异步输出结果，避免死锁
-        // 使用TAP模式，避免EAP、APM模式
-        private void HashFiles(string[] files)
+        // 尽量使用TAP模式，避免EAP、APM模式
+        private async void HashFiles(string[] files)
         {
             // 判断文件，不支持目录
             if ((files == null) || (files.Length <= 0)) return;
             FirstClear();
+            isProgressing = true;
             UpdateGUI(isBusy: true);
             // 添加到处理队列
             foreach (string f in files)
             {
-                var a = new GetFileHash(hashType,f);
-                textBox_Stream.Text += a.FileName + nl + a.HashResult + nl;
-
-                
+                FileTask(f);
             }
-            // 基于所有任务（处理队列）的进度条更新
         }
 
-//        private async Task HashFileAsync(string file)
-//        {
-//#if DEBUG
-//            var timer = new System.Diagnostics.Stopwatch();
-//            timer.Start();
-//#endif
-//            var result = new GetHash.FileResult();
-//            result = GetHash.GetFileHash(hashType, file, isFullPath);
-//            // 显示文本
-//            if (result.hash != GetHash.FILE_ERROR)
-//            {
-//                textBox_HashCode.Text = result.hash;
-//            }
-//#if DEBUG
-//            timer.Stop();
-//            textBox_Stream.Text += "耗时：" + timer.Elapsed.ToString();
-//#endif
-//            textBox_Stream.Text += result.path + nl + result.hash + nl + nl;
-//        }
-        
         // 停止任务
         private void StopHashing()
         {
@@ -92,21 +116,28 @@ namespace FileHasherWPF
             if (isBusy)
             {
                 button_Stop.IsEnabled = true;
+                checkBox_IsText.IsEnabled = false;
                 progressBar.Visibility = Visibility.Visible;
             }
             else
             {
-                button_Stop.IsEnabled = false;
-                progressBar.Visibility = Visibility.Collapsed;
-                // 滚动到最后一行
-                textBox_Stream.Focus();
-                textBox_Stream.CaretIndex = textBox_Stream.Text.Length; // 插入光标到末尾
-                textBox_Stream.ScrollToEnd();
+                if (!isProgressing)
+                {
+                    button_Stop.IsEnabled = false;
+                    checkBox_IsText.IsEnabled = true;
+                    progressBar.Visibility = Visibility.Collapsed;
+                    // 滚动到最后一行
+                    textBox_Stream.Focus();
+                    textBox_Stream.CaretIndex = textBox_Stream.Text.Length; // 插入光标到末尾
+                    textBox_Stream.ScrollToEnd();
+                }
             }
         }
+        #endregion
 
+        #region 文本框的逻辑
         // 文本框的首次清空
-        public void FirstClear()
+        private void FirstClear()
         {
             if (firstClick)
             {
@@ -115,53 +146,34 @@ namespace FileHasherWPF
             }
         }
 
-        // 计算文本的哈希
-        private void HashText()
+        // 文本框被聚焦
+        private void TextBox_Stream_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (!firstClick && isTextMode && (textBox_Stream.Text != ""))
+            FirstClear();
+        }
+        // 文本框文本有变动
+        private void TextBox_Stream_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!firstClick && isTextMode)
             {
                 textBox_HashCode.Text = GetHash.GetStringHash(hashType, textBox_Stream.Text);
             }
         }
         #endregion
 
-        #region GUI交互逻辑
-        // 文本框键入
-        private void TextBox_Stream_GotFocus(object sender, RoutedEventArgs e)
-        {
-            FirstClear();
-        }
-        private void TextBox_Stream_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (textBox_Stream.IsFocused)
-                HashText();
-        }
-
-        // 打开文件对话框
-        private void Button_GetHash_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog
-            {
-                Multiselect = true
-            };
-            if (openFileDialog.ShowDialog() == true)
-            {
-                HashFiles(openFileDialog.FileNames);
-            }
-        }
-
-        // 拖放文件到窗口
+        #region 拖放的逻辑
         /*
-         WPF中，TextBox组件无法直接在DragEnter事件中接受文件的拖放，而是显示为“禁止”的鼠标指针，
-         这是WPF的特性，是优点，TextBox理应只接受String。
-         但当不需要这种特性时，使用【PreviewDragEnter】事件响应即可完美解决，简洁优雅。
-         WinForm不用考虑这个特性，但却要手动处理逻辑分层的问题。
-         WinForm使用的是Direct Event，而WPF则有Bubbling Event与Tunneling Event，Preview开头的属于后者
-         https://docs.microsoft.com/en-us/dotnet/framework/wpf/advanced/routed-events-overview
-         在研究过程中，曾用过一个丑陋的workaround：
-         textBox_Stream.IsHitTestVisible = false
-         这样实际上拖放时是到其下层的窗体，但也使得TextBox不能鼠标操作，变成只能看不能点的组件。
+            WPF中，TextBox组件无法直接在DragEnter事件中接受文件的拖放，而是显示为“禁止”的鼠标指针，
+            这是WPF的特性，是优点，TextBox理应只接受String。
+            但当不需要这种特性时，使用【PreviewDragEnter】事件响应即可完美解决，简洁优雅。
+            WinForm不用考虑这个特性，但却要手动处理逻辑分层的问题。
+            WinForm使用的是Direct Event，而WPF则有Bubbling Event与Tunneling Event，Preview开头的属于后者
+            https://docs.microsoft.com/en-us/dotnet/framework/wpf/advanced/routed-events-overview
+            在研究过程中，曾用过一个丑陋的workaround：
+            textBox_Stream.IsHitTestVisible = false
+            这样实际上拖放时是到其下层的窗体，但也使得TextBox不能鼠标操作，变成只能看不能点的组件。
         */
+        // 拖放文件到窗口
         private void OnDragEnter(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -184,7 +196,20 @@ namespace FileHasherWPF
         }
         #endregion
 
-        #region 其它按钮的逻辑
+        #region 各按钮的点击
+        // 打开文件对话框
+        private void Button_GetHash_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                HashFiles(openFileDialog.FileNames);
+            }
+        }
+
         // 停止操作
         private void Button_Stop_Click(object sender, RoutedEventArgs e)
         {
@@ -234,7 +259,7 @@ namespace FileHasherWPF
         }
         #endregion
 
-        #region 单选框的逻辑
+        #region 单选框的事件逻辑
         private void RadioButton_MD5_Checked(object sender, RoutedEventArgs e)
         {
             textBox_HashCodeForCompare.MaxLength = 32;
@@ -257,7 +282,7 @@ namespace FileHasherWPF
         }
         #endregion
 
-        #region 复选框的逻辑
+        #region 复选框的事件逻辑
         private void CheckBox_IsText_Checked(object sender, RoutedEventArgs e)
         {
             isTextMode = true;
