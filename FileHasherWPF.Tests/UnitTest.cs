a43913441a357d9148435adbc60db6cab46b6e70
Nt.gm79sp <gm79sp@qq.com>
2019-11-10 22:25:00 +0800

    Add xUnit Test

---
+++ FileHasherWPF.Tests/UnitTest.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace FileHasherWPF.Tests
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void TestStringMD5()
+        {
+
+        }
+        [Fact]
+        public void TestStringSHA1()
+        {
+
+        }
+        [Fact]
+        public void TestStringSHA256()
+        {
+
+        }
+        [Fact]
+        public void TestStringSHA512()
+        {
+
+        }
+    }
+}
